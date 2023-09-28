4ffd38fcbc3eb4d7e06d3f7875ffc105bba96964
zerodetail <zerodetail@gmail.com>
2023-09-29 08:26:46 +1000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Zero too many linked lists flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            cargo-watch
+            rust-analyzer
+            rust-bin.stable.latest.default
+          ];
+
+          shellHook = ''
+            export DEV_SHELL=1
+          '';
+        };
+      }
+    );
+}
